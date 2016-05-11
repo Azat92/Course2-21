47b07de4ade6cebe97351c3ec67ec692a280c44f
Azat Almeev <azat.almeev@gmail.com>
2016-05-11 18:14:51 +0300

    - Lesson code

--- Lesson21/ViewController.m
+++ Lesson21/ViewController.m
@@ -7,6 +7,12 @@
 //
 
 #import "ViewController.h"
+#import <BlocksKit+MessageUI.h>
+#import <BlocksKit+UIKit.h>
+
+@interface NSNumber (Extended)
+- (NSArray *)take;
+@end
 
 @interface ViewController ()
 
@@ -16,12 +22,104 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    NSArray *input = [@5 take];
+//    NSLog(@"%@", [self incrementArrayBy1:input]);
+//    NSLog(@"%@", [input bk_select:^BOOL(NSNumber *obj) {
+//        return obj.integerValue % 2 == 0;
+//    }]);
+//    NSLog(@"%@", [input bk_reduce:@0 withBlock:^id(NSNumber *sum, NSNumber *obj) {
+//        return @([sum integerValue] + [obj integerValue]);
+//    }]);
+//    NSLog(@"%@", [input bk_reduce:@"" withBlock:^id(id sum, id obj) {
+//        return [NSString stringWithFormat:@"%@%@", sum, obj];
+//    }]);
+//    NSDictionary *keyValues = @{ @"key1" : @1, @"key2" : @2, @"key3" : @3 };
+//    NSLog(@"%@", [[keyValues bk_map:^id(id key, id obj) {
+//        return @([obj integerValue] * 2);
+//    }] bk_select:^BOOL(id key, id obj) {
+//        return [obj integerValue] > 5;
+//    }]);
+    
+    
+    NSArray *buttons = [input bk_map:^id(id obj) {
+        UIButton *button = [UIButton buttonWithType:UIButtonTypeSystem];
+        [button setTitle:[NSString stringWithFormat:@"Btn %@", obj] forState:UIControlStateNormal];
+        NSInteger index = [obj integerValue] - 1;
+        button.frame = CGRectMake(index * 100 + 20, 40, 90, 35);
+//        button.tag = index;
+//        [button addTarget:self action:@selector(buttonDidTap:) forControlEvents:UIControlEventTouchUpInside];
+        [button bk_addEventHandler:^(UIButton *sender) {
+            NSLog(@"%@", @(index));
+        } forControlEvents:UIControlEventTouchUpInside];
+        [button bk_removeEventHandlersForControlEvents:UIControlEventTouchUpInside];
+        [button bk_addEventHandler:^(UIButton *sender) {
+            NSLog(@"%@", @(index * 2));
+        } forControlEvents:UIControlEventTouchUpInside];
+        return button;
+    }];
+    [buttons bk_each:^(id obj) {
+        [self.view addSubview:obj];
+    }];
 }
 
-- (void)didReceiveMemoryWarning {
-    [super didReceiveMemoryWarning];
-    // Dispose of any resources that can be recreated.
+- (IBAction)buttonDidTap:(UIButton *)sender {
+    NSLog(@"%@", @(sender.tag));
+}
+
+- (NSArray <NSNumber *>*)incrementArrayBy1:(NSArray <NSNumber *>*)input {
+    return [input bk_map:^id(NSNumber *obj) {
+        return @(obj.integerValue + 1);
+    }];
+}
+
+- (NSArray <NSNumber *>*)getLengthsOfElements:(NSArray <NSString *>*)input {
+    return [input bk_map:^id(NSString *obj) {
+        return @(obj.length);
+    }];
 }
 
 @end
+
+@implementation NSNumber (Extended)
+
+- (NSArray *)take {
+    NSMutableArray *res = [NSMutableArray new];
+    for (int i = 1; i <= self.integerValue; i++)
+        [res addObject:@(i)];
+    return res.copy;
+}
+
+@end
+
+//@interface NSArray (Extended)
+//- (NSArray *)modifyUsingBlock:(id (^)(id element))block;
+//@end
+//
+//@implementation NSArray (Extended)
+//
+//- (NSArray *)modifyUsingBlock:(id (^)(id element))block {
+//    NSMutableArray *result = [NSMutableArray new];
+//    [self enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL * _Nonnull stop) {
+//        [result addObject:block(obj)];
+//    }];
+//    return result.copy;
+//}
+//
+//- (NSArray *)selectUsingBlock:(BOOL (^)(id element))block {
+//    NSMutableArray *result = [NSMutableArray new];
+//    [self enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL * _Nonnull stop) {
+//        if (block(obj))
+//            [result addObject:obj];
+//    }];
+//    return result.copy;
+//}
+//
+//- (id)shrinkUsingInitial:(id)initial andBlock:(id (^)(id current, id element))block {
+//    __block id result = initial;
+//    [self enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
+//        result = block(result, obj);
+//    }];
+//    return result;
+//}
+//
+//@end
