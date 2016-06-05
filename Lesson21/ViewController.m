64f3a06d15f0a6030e0fa1fc4ff8be979e980830
Timofeeva_11-405 <manya1912@mail.ru>
2016-06-05 18:33:30 +0300

    HM  "Blocks" added.
    Можно прибавлять и умножать на числа от 1 до 5, также выбрать только
    четные или нечетные числа из текущего. Есть кнопка сброса.

--- Lesson21/ViewController.m
+++ Lesson21/ViewController.m
@@ -15,56 +15,89 @@
 @end
 
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UINavigationItem *navigationItem;
 
 @end
 
 @implementation ViewController
+- (IBAction)update:(id)sender {
+    self.navigationItem.title=@"0";
+}
 
 - (void)viewDidLoad {
     [super viewDidLoad];
     NSArray *input = [@5 take];
-//    NSLog(@"%@", [self incrementArrayBy1:input]);
-//    NSLog(@"%@", [input bk_select:^BOOL(NSNumber *obj) {
-//        return obj.integerValue % 2 == 0;
-//    }]);
-//    NSLog(@"%@", [input bk_reduce:@0 withBlock:^id(NSNumber *sum, NSNumber *obj) {
-//        return @([sum integerValue] + [obj integerValue]);
-//    }]);
-//    NSLog(@"%@", [input bk_reduce:@"" withBlock:^id(id sum, id obj) {
-//        return [NSString stringWithFormat:@"%@%@", sum, obj];
-//    }]);
-//    NSDictionary *keyValues = @{ @"key1" : @1, @"key2" : @2, @"key3" : @3 };
-//    NSLog(@"%@", [[keyValues bk_map:^id(id key, id obj) {
-//        return @([obj integerValue] * 2);
-//    }] bk_select:^BOOL(id key, id obj) {
-//        return [obj integerValue] > 5;
-//    }]);
-    
-    
+
+    UILabel *multLabel = [UILabel new];
+    [multLabel setText:@"Mult"];
+    [multLabel setFrame:CGRectMake(10, 60, 50, 30)];
+    [self.view addSubview:multLabel];
     NSArray *buttons = [input bk_map:^id(id obj) {
-        UIButton *button = [UIButton buttonWithType:UIButtonTypeSystem];
-        [button setTitle:[NSString stringWithFormat:@"Btn %@", obj] forState:UIControlStateNormal];
-        NSInteger index = [obj integerValue] - 1;
-        button.frame = CGRectMake(index * 100 + 20, 40, 90, 35);
-//        button.tag = index;
-//        [button addTarget:self action:@selector(buttonDidTap:) forControlEvents:UIControlEventTouchUpInside];
-        [button bk_addEventHandler:^(UIButton *sender) {
-            NSLog(@"%@", @(index));
+        UIButton *addButton = [UIButton buttonWithType:UIButtonTypeSystem];
+        [addButton setTitle:[NSString stringWithFormat:@"%@", obj] forState:UIControlStateNormal];
+        NSInteger index = [obj integerValue];
+        addButton.frame = CGRectMake(50*index +40, 60, 90, 30);
+        [addButton bk_addEventHandler:^(UIButton *sender) {
+            [self.navigationItem setTitle:[NSString stringWithFormat:@"%ld", [self.navigationItem.title integerValue]* index]];
         } forControlEvents:UIControlEventTouchUpInside];
-        [button bk_removeEventHandlersForControlEvents:UIControlEventTouchUpInside];
-        [button bk_addEventHandler:^(UIButton *sender) {
-            NSLog(@"%@", @(index * 2));
-        } forControlEvents:UIControlEventTouchUpInside];
-        return button;
+        return addButton;
     }];
     [buttons bk_each:^(id obj) {
         [self.view addSubview:obj];
     }];
+    
+    UILabel *addLabel = [UILabel new];
+    [addLabel setText:@"Add"];
+    [addLabel setFrame:CGRectMake(10, 100, 50, 30)];
+    [self.view addSubview:addLabel];
+    NSArray *addButtons = [input bk_map:^id(id obj) {
+        UIButton *addButton = [UIButton buttonWithType:UIButtonTypeSystem];
+        [addButton setTitle:[NSString stringWithFormat:@"%@", obj] forState:UIControlStateNormal];
+        NSInteger index = [obj integerValue];
+        addButton.frame = CGRectMake(50*index +40, 100, 90, 30);
+        [addButton bk_addEventHandler:^(UIButton *sender) {
+            [self.navigationItem setTitle:[NSString stringWithFormat:@"%ld", [self.navigationItem.title integerValue]+ index]];
+        } forControlEvents:UIControlEventTouchUpInside];
+        return addButton;
+    }];
+    [addButtons bk_each:^(id obj) {
+        [self.view addSubview:obj];
+    }];
+
+    UILabel *filterLabel = [UILabel new];
+    [filterLabel setText:@"Filter"];
+    [filterLabel setFrame:CGRectMake(10, 140, 50, 30)];
+    [self.view addSubview:filterLabel];
+    NSArray *filterButtons = [[@2 take] bk_map:^id(id obj) {
+        UIButton *addButton = [UIButton buttonWithType:UIButtonTypeSystem];
+        [addButton setTitle:[NSString stringWithFormat:@"%@", obj] forState:UIControlStateNormal];
+        NSInteger index = [obj integerValue];
+        addButton.frame = CGRectMake(50*index +40, 140, 90, 30);
+        [addButton bk_addEventHandler:^(UIButton *sender) {
+           __block NSString *title = self.navigationItem.title.copy;
+            NSMutableArray *value = [NSMutableArray new];
+            for (int i = 0; i < title.length ;i++) [value addObject: [NSString stringWithFormat:@"%c",[title characterAtIndex:i] ]];
+           value = [value bk_select:^BOOL(id obj) {
+                int i = index == 1 ? 1 : 0;
+                return [obj integerValue]%2==i;
+            }];
+            title = @"";
+            [value bk_each:^(id obj) {
+                title = [NSString stringWithFormat:@"%@%@",title, obj];
+            }];
+            title =  title.length> 0  ? title : @"0" ;
+            [self.navigationItem setTitle:title];
+        } forControlEvents:UIControlEventTouchUpInside];
+        return addButton;
+    }];
+    [filterButtons bk_each:^(id obj) {
+        [self.view addSubview:obj];
+    }];
+
+    
+    
 }
 
-- (IBAction)buttonDidTap:(UIButton *)sender {
-    NSLog(@"%@", @(sender.tag));
-}
 
 - (NSArray <NSNumber *>*)incrementArrayBy1:(NSArray <NSNumber *>*)input {
     return [input bk_map:^id(NSNumber *obj) {
@@ -91,35 +124,3 @@
 
 @end
 
-//@interface NSArray (Extended)
-//- (NSArray *)modifyUsingBlock:(id (^)(id element))block;
-//@end
-//
-//@implementation NSArray (Extended)
-//
-//- (NSArray *)modifyUsingBlock:(id (^)(id element))block {
-//    NSMutableArray *result = [NSMutableArray new];
-//    [self enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL * _Nonnull stop) {
-//        [result addObject:block(obj)];
-//    }];
-//    return result.copy;
-//}
-//
-//- (NSArray *)selectUsingBlock:(BOOL (^)(id element))block {
-//    NSMutableArray *result = [NSMutableArray new];
-//    [self enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL * _Nonnull stop) {
-//        if (block(obj))
-//            [result addObject:obj];
-//    }];
-//    return result.copy;
-//}
-//
-//- (id)shrinkUsingInitial:(id)initial andBlock:(id (^)(id current, id element))block {
-//    __block id result = initial;
-//    [self enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
-//        result = block(result, obj);
-//    }];
-//    return result;
-//}
-//
-//@end
