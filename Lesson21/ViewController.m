052f6c0954e2aa552f155c3a984642319b44fe88
Ryazapov <eduard.ryazapov@gmail.com>
2016-06-06 20:38:02 +0300

    complete task

--- Lesson21/ViewController.m
+++ Lesson21/ViewController.m
@@ -10,11 +10,21 @@
 #import <BlocksKit+MessageUI.h>
 #import <BlocksKit+UIKit.h>
 
-@interface NSNumber (Extended)
-- (NSArray *)take;
-@end
-
 @interface ViewController ()
+
+@property (weak, nonatomic) IBOutlet UITextField *addNumberTextField;
+@property (weak, nonatomic) IBOutlet UIButton *addNumberButton;
+@property (weak, nonatomic) IBOutlet UIButton *removeNumbersButton;
+@property (weak, nonatomic) IBOutlet UILabel *numbersLabel;
+@property (weak, nonatomic) IBOutlet UITextField *sumTextField;
+@property (weak, nonatomic) IBOutlet UIButton *sumButton;
+@property (weak, nonatomic) IBOutlet UITextField *multTextField;
+@property (weak, nonatomic) IBOutlet UIButton *multButton;
+@property (weak, nonatomic) IBOutlet UIButton *evenButton;
+@property (weak, nonatomic) IBOutlet UIButton *oddButton;
+
+@property NSMutableArray<NSNumber *> *numbers;
+@property NSNumberFormatter *formatter;
 
 @end
 
@@ -22,104 +32,72 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    NSArray *input = [@5 take];
-//    NSLog(@"%@", [self incrementArrayBy1:input]);
-//    NSLog(@"%@", [input bk_select:^BOOL(NSNumber *obj) {
-//        return obj.integerValue % 2 == 0;
-//    }]);
-//    NSLog(@"%@", [input bk_reduce:@0 withBlock:^id(NSNumber *sum, NSNumber *obj) {
-//        return @([sum integerValue] + [obj integerValue]);
-//    }]);
-//    NSLog(@"%@", [input bk_reduce:@"" withBlock:^id(id sum, id obj) {
-//        return [NSString stringWithFormat:@"%@%@", sum, obj];
-//    }]);
-//    NSDictionary *keyValues = @{ @"key1" : @1, @"key2" : @2, @"key3" : @3 };
-//    NSLog(@"%@", [[keyValues bk_map:^id(id key, id obj) {
-//        return @([obj integerValue] * 2);
-//    }] bk_select:^BOOL(id key, id obj) {
-//        return [obj integerValue] > 5;
-//    }]);
+    self.numbers = [NSMutableArray new];
+    self.formatter = [NSNumberFormatter new];
+    [self.addNumberButton bk_addEventHandler:^(UIButton *sender) {
+        if (self.numbers.count < 10) {
+            NSNumber *number = [self.formatter numberFromString:self.addNumberTextField.text];
+            [self.numbers addObject:number];
+            if (self.numbers.count < 10) {
+                self.numbersLabel.text = [NSString stringWithFormat:@"%@ %@,", self.numbersLabel.text, number];
+            } else {
+                self.numbersLabel.text = [NSString stringWithFormat:@"%@ %@", self.numbersLabel.text, number];
+            }
+        } else {
+            self.addNumberTextField.text = @"";
+            self.addNumberTextField.placeholder = @"Максимум чисел.";
+        }
+    } forControlEvents:UIControlEventTouchUpInside];
     
+    [self.removeNumbersButton bk_addEventHandler:^(UIButton *sender) {
+        [self.numbers removeAllObjects];
+        self.numbersLabel.text = @"Числа: ";
+    } forControlEvents:UIControlEventTouchUpInside];
     
-    NSArray *buttons = [input bk_map:^id(id obj) {
-        UIButton *button = [UIButton buttonWithType:UIButtonTypeSystem];
-        [button setTitle:[NSString stringWithFormat:@"Btn %@", obj] forState:UIControlStateNormal];
-        NSInteger index = [obj integerValue] - 1;
-        button.frame = CGRectMake(index * 100 + 20, 40, 90, 35);
-//        button.tag = index;
-//        [button addTarget:self action:@selector(buttonDidTap:) forControlEvents:UIControlEventTouchUpInside];
-        [button bk_addEventHandler:^(UIButton *sender) {
-            NSLog(@"%@", @(index));
-        } forControlEvents:UIControlEventTouchUpInside];
-        [button bk_removeEventHandlersForControlEvents:UIControlEventTouchUpInside];
-        [button bk_addEventHandler:^(UIButton *sender) {
-            NSLog(@"%@", @(index * 2));
-        } forControlEvents:UIControlEventTouchUpInside];
-        return button;
-    }];
-    [buttons bk_each:^(id obj) {
-        [self.view addSubview:obj];
-    }];
+    [self.sumButton bk_addEventHandler:^(UIButton *sender) {
+        self.numbers = [NSMutableArray arrayWithArray:[self.numbers bk_map:^NSNumber*(NSNumber *obj) {
+            NSNumber *number = [self.formatter numberFromString:self.sumTextField.text];
+            return [NSNumber numberWithInteger:([obj integerValue] + [number integerValue])];
+        }]];
+        [self updateLabel];
+    } forControlEvents:UIControlEventTouchUpInside];
+    
+    [self.multButton bk_addEventHandler:^(UIButton *sender) {
+        self.numbers = [NSMutableArray arrayWithArray:[self.numbers bk_map:^NSNumber*(NSNumber *obj) {
+            NSNumber *number = [self.formatter numberFromString:self.multTextField.text];
+            return [NSNumber numberWithInteger:([obj integerValue] * [number integerValue])];
+        }]];
+        [self updateLabel];
+    } forControlEvents:UIControlEventTouchUpInside];
+    
+    [self.evenButton bk_addEventHandler:^(UIButton *sender) {
+        self.numbers = [NSMutableArray arrayWithArray:[self.numbers bk_select:^BOOL(NSNumber *obj) {
+            return obj.integerValue % 2 == 0;
+        }]];
+        [self updateLabel];
+    } forControlEvents:UIControlEventTouchUpInside];
+    
+    [self.oddButton bk_addEventHandler:^(UIButton *sender) {
+        self.numbers = [NSMutableArray arrayWithArray:[self.numbers bk_select:^BOOL(NSNumber *obj) {
+            return obj.integerValue % 2 != 0;
+        }]];
+        [self updateLabel];
+    } forControlEvents:UIControlEventTouchUpInside];
 }
 
-- (IBAction)buttonDidTap:(UIButton *)sender {
-    NSLog(@"%@", @(sender.tag));
-}
-
-- (NSArray <NSNumber *>*)incrementArrayBy1:(NSArray <NSNumber *>*)input {
-    return [input bk_map:^id(NSNumber *obj) {
-        return @(obj.integerValue + 1);
-    }];
-}
-
-- (NSArray <NSNumber *>*)getLengthsOfElements:(NSArray <NSString *>*)input {
-    return [input bk_map:^id(NSString *obj) {
-        return @(obj.length);
-    }];
+- (void)updateLabel {
+    self.numbersLabel.text = @"Числа: ";
+    if (self.numbers.count == 10) {
+        for (int i = 0; i < 10; i++) {
+            self.numbersLabel.text = [NSString stringWithFormat:@"%@ %@,", self.numbersLabel.text, self.numbers[i]];
+        }
+        self.numbersLabel.text = [NSString stringWithFormat:@"%@ %@", self.numbersLabel.text, self.numbers[9]];
+    } else {
+        for (int i = 0; i < self.numbers.count; i++) {
+            self.numbersLabel.text = [NSString stringWithFormat:@"%@ %@,", self.numbersLabel.text, self.numbers[i]];
+        }
+    }
 }
 
 @end
 
-@implementation NSNumber (Extended)
-
-- (NSArray *)take {
-    NSMutableArray *res = [NSMutableArray new];
-    for (int i = 1; i <= self.integerValue; i++)
-        [res addObject:@(i)];
-    return res.copy;
-}
-
-@end
-
-//@interface NSArray (Extended)
-//- (NSArray *)modifyUsingBlock:(id (^)(id element))block;
-//@end
-//
-//@implementation NSArray (Extended)
-//
-//- (NSArray *)modifyUsingBlock:(id (^)(id element))block {
-//    NSMutableArray *result = [NSMutableArray new];
-//    [self enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL * _Nonnull stop) {
-//        [result addObject:block(obj)];
-//    }];
-//    return result.copy;
-//}
-//
-//- (NSArray *)selectUsingBlock:(BOOL (^)(id element))block {
-//    NSMutableArray *result = [NSMutableArray new];
-//    [self enumerateObjectsUsingBlock:^(id obj, NSUInteger idx, BOOL * _Nonnull stop) {
-//        if (block(obj))
-//            [result addObject:obj];
-//    }];
-//    return result.copy;
-//}
-//
-//- (id)shrinkUsingInitial:(id)initial andBlock:(id (^)(id current, id element))block {
-//    __block id result = initial;
-//    [self enumerateObjectsUsingBlock:^(id  _Nonnull obj, NSUInteger idx, BOOL * _Nonnull stop) {
-//        result = block(result, obj);
-//    }];
-//    return result;
-//}
-//
-//@end
